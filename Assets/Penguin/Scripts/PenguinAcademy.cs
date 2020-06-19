e76799fb88fff8fabd870e42dbc116ef79731a60
ifrah6597 <65050592+ifrah6597@users.noreply.github.com>
2020-06-18 23:26:13 -0700

    updated

---
+++ Assets/Penguin/Scripts/PenguinAcademy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class PenguinAcademy : Academy
+{
+    //summary
+    //gets/sets the current fish speed
+    //summary
+
+        public float FishSpeed
+    {
+        get;
+        private set;
+
+       
+    }
+    //summary
+    //gets/sets the current acceptable feed radius
+    //summary
+    public float FeedRadius
+{
+        get;
+        private set;
+    }
+    //summary
+    //called when the acdemy first get initialized
+    //summary
+    public override void InitializeAcademy()
+    {
+        FishSpeed = 0f;
+        FeedRadius = 0f;
+        //set-up the code to be called every-time the fish-speed parameter changes
+        //during curriculum learning
+        FloatProperties.RegisterCallback("fish_speed", f =>
+         {
+             FishSpeed = f;
+         });
+        //set-up the code to be called every-time the fish-radius parameter changes
+        //during curriculum learning
+        FloatProperties.RegisterCallback("fish_radius", f =>
+        {
+            FeedRadius = f;
+        });
+
+    }
+}
